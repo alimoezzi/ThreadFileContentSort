b5c8ac27e6ce2c95945cabfc19f5a43a3c21cbe1
ALi <s.arme@outlook.com>
2020-09-25 15:17:44 +0330

    ✨ Run each file sorting task in a seperate thread

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
 import os
+import threading
 
 
 def sortIntFile(filename: str):
     lines = []
     with open(filename, 'rt') as file:
         lines = list(file.readlines())
-    lines = lines.sort(key=lambda x: int(x))
+    lines.sort(key=lambda x: int(x.split('\n')[0]))
     with  open(filename, 'wt') as file:
-        file.write('\n'.join(lines))
+        file.write(''.join(lines))
         file.flush()
-
 
 
 if __name__ == '__main__':
     for i in os.scandir('intFiles/'):
         try:
-            sortIntFile(i.path)
+            t = threading.Thread(target=sortIntFile, args=(i.path,), name=f'sort {i.path}')
+            t.start()
         except Exception as e:
             print(e)
             os.system('pause')
