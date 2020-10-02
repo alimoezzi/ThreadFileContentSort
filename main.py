49fc6cda7206f98adf0fea6d59889af9d92c187b
ALi <s.arme@ya.ru>
2020-10-02 21:17:46 +0330

    ✨ Redesign program to use custome number of threads

--- main.py
+++ main.py
@@ -1,27 +1,31 @@
 import os
 import threading
+import numpy as np
 from createRandomFile import createRandomIntFile
 
 
-def sortIntFile(filename: str):
-    lines = []
-    with open(filename, 'rt') as file:
-        lines = list(file.readlines())
-    lines.sort(key=lambda x: int(x.split('\n')[0]))
-    with  open(filename, 'wt') as file:
-        file.write(''.join(lines))
-        file.flush()
+def sortIntFile(filenames: list):
+    for filename in filenames:
+        lines = []
+        with open(filename, 'rt') as file:
+            lines = list(file.readlines())
+        lines.sort(key=lambda x: int(x.split('\n')[0]))
+        with  open(filename, 'wt') as file:
+            file.write(''.join(lines))
+            file.flush()
 
-    print(f'\nThread {threading.currentThread().getName()} exiting')
+        print(f'\nThread {threading.currentThread().getName()} exiting')
 
 
 if __name__ == '__main__':
     createRandomIntFile()
     print('Random integer files created.')
     os.system('pause')
-    for i in os.scandir('intFiles/'):
+    n = input("Enter number of workers: ")
+    l = [i.path for i in os.scandir('intFiles/') if '.keep' not in i.path]
+    for i in np.array_split(l, int(n)):
         try:
-            t = threading.Thread(target=sortIntFile, args=(i.path,), name=f'sort {i.path}')
+            t = threading.Thread(target=sortIntFile, args=(i,), name=f'sort {i}')
             print(f'Thread {t.getName()} about to start')
             t.start()
         except Exception as e:
